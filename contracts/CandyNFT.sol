bfeb9ff8e5c8eb9ba475869822edc60ee100ed1e
NorVirae <norbertmbafrank@gmail.com>
2023-06-20 10:49:12 +0100

    add initial

---
+++ contracts/CandyNFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract CandyNFT is ERC721URIStorage {
+    constructor()   {
+        
+    }
+}
